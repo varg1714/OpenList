46453d6a7e7286d2aec9fa3dc028ceaa6ee34a3a
varg247 <varg247@gmail.com>
2026-07-17 22:02:33 +0800

    refactor(fc2): name WhatsLink screenshots
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- drivers/fc2/types.go
+++ drivers/fc2/types.go
@@ -1,16 +1,18 @@
 package fc2
 
 type WhatLinkInfo struct {
-	Error       string `json:"error"`
-	Type        string `json:"type"`
-	FileType    string `json:"file_type"`
-	Name        string `json:"name"`
-	Size        int64  `json:"size"`
-	Count       int    `json:"count"`
-	Screenshots []struct {
-		Time       int    `json:"time"`
-		Screenshot string `json:"screenshot"`
-	} `json:"screenshots"`
+	Error       string               `json:"error"`
+	Type        string               `json:"type"`
+	FileType    string               `json:"file_type"`
+	Name        string               `json:"name"`
+	Size        int64                `json:"size"`
+	Count       int                  `json:"count"`
+	Screenshots []WhatLinkScreenshot `json:"screenshots"`
+}
+
+type WhatLinkScreenshot struct {
+	Time       int    `json:"time"`
+	Screenshot string `json:"screenshot"`
 }
 
 type MakeDirParam struct {
