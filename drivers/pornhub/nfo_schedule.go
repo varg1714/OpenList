44bdef3f350ba94f7fc2369262b1330c54f3a005
varg247 <varg247@gmail.com>
2026-07-26 11:15:35 +0800

    fix(pornhub): omit code from NFO titles
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- drivers/pornhub/nfo_schedule.go
+++ drivers/pornhub/nfo_schedule.go
@@ -6,10 +6,10 @@ var syncPornhubMediaNFOs = virtual_file.SyncMediaNFOs
 
 func (d *Pornhub) syncConfiguredNFOs() error {
 	if d.RefreshNfo {
-		return syncPornhubMediaNFOs(d.ID, DriverName, true)
+		return syncPornhubMediaNFOs(d.ID, DriverName, virtual_file.MediaNFOSyncOptions{Force: true})
 	}
 	if d.SyncNfo {
-		return syncPornhubMediaNFOs(d.ID, DriverName, false)
+		return syncPornhubMediaNFOs(d.ID, DriverName, virtual_file.MediaNFOSyncOptions{})
 	}
 	return nil
 }
--- drivers/pornhub/nfo_schedule_test.go
+++ drivers/pornhub/nfo_schedule_test.go
@@ -3,17 +3,18 @@ package pornhub
 import (
 	"testing"
 
+	"github.com/OpenListTeam/OpenList/v4/drivers/virtual_file"
 	"github.com/stretchr/testify/require"
 )
 
 func TestPornhubNFOFlagsPreferForcedRefreshWhenBothEnabled(t *testing.T) {
 	oldSync := syncPornhubMediaNFOs
 	t.Cleanup(func() { syncPornhubMediaNFOs = oldSync })
-	var forces []bool
-	syncPornhubMediaNFOs = func(storageID uint, source string, force bool) error {
+	var options []virtual_file.MediaNFOSyncOptions
+	syncPornhubMediaNFOs = func(storageID uint, source string, option virtual_file.MediaNFOSyncOptions) error {
 		require.Equal(t, uint(83), storageID)
 		require.Equal(t, DriverName, source)
-		forces = append(forces, force)
+		options = append(options, option)
 		return nil
 	}
 
@@ -22,5 +23,5 @@ func TestPornhubNFOFlagsPreferForcedRefreshWhenBothEnabled(t *testing.T) {
 	err := driver.syncConfiguredNFOs()
 
 	require.NoError(t, err)
-	require.Equal(t, []bool{true}, forces)
+	require.Equal(t, []virtual_file.MediaNFOSyncOptions{{Force: true}}, options)
 }
