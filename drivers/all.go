9f1bfa27b5373eaf53c9390dbc90d6a81060eb4e
varg247 <varg247@gmail.com>
2026-08-01 15:58:14 +0800

    feat(cache): register cache driver

--- drivers/all.go
+++ drivers/all.go
@@ -22,6 +22,7 @@ import (
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/baidu_netdisk"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/baidu_photo"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/chaoxing"
+	_ "github.com/OpenListTeam/OpenList/v4/drivers/cache"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/chunk"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/cloudreve"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/cloudreve_v4"
